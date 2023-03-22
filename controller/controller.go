bcf2f130c0b6917c27e2c1188d351a1e3dfca937
adachiryuji <ryuji.adachi@rakus-partners.co.jp>
2023-03-23 05:35:10 +0900

    修正

--- controller/controller.go
+++ controller/controller.go
@@ -17,28 +17,37 @@ type Controller struct {
 }
 
 func NewController() Controller {
+	//Controllerの構造体を返す
 	return Controller{}
 }
 
 func (c Controller) Execute() {
 	fmt.Println("Execute")
-	ctx := context.Background()
-	gin.SetMode(gin.ReleaseMode)
+
+	ctx := context.Background() //空のContextを生成
+
+	gin.SetMode(gin.ReleaseMode) //Ginのデバックモードを無効にする
 	router := gin.Default()
 
-	conn, err := grpc.Dial("localhost:8082", grpc.WithInsecure())
+	conn, err := grpc.Dial("localhost:8082", grpc.WithInsecure()) //指定したターゲット(localhost:8082)へ接続を作成している。 rpc.WithInsecure()は「認証をしないで接続する」という指定をしている。
 	if err != nil {
 		log.Fatalf("fail connect:%v", err)
 	}
-	defer conn.Close()
-	client := pb.NewBookServiceClient(conn)
+	defer conn.Close()                      //一通りの処理がclient側で完了したら接続を閉じる
+	client := pb.NewBookServiceClient(conn) //gRPCクライアントを生成している(gRPCクライアントを「localhost:8082」に接続)
 
+	//パスとメソッドを紐づけている。
+	//gRPC通信をする為に引数には必ずgRPCクライアントを持たせている。
+
+	//HTTP通信でGETメソッドを指定している
 	router.GET("/api/books", func(c *gin.Context) {
-		getBooks(ctx, c, client)
+		getBooks(ctx, c, client) //gin.Contextを引数に持たせてメソッドを呼び出している
 	})
 	router.GET("/api/book/:id", func(c *gin.Context) {
+		//パラメーターからidを取得する
 		getBook(ctx, c, client)
 	})
+	//HTTP通信でPOSTメソッドを指定している
 	router.POST("api/book/create", func(c *gin.Context) {
 		createBook(ctx, c, client)
 	})
@@ -49,23 +58,26 @@ func (c Controller) Execute() {
 		deleteBook(ctx, c, client)
 	})
 
-	router.Run(":8080")
+	router.Run(":8080") //ルーターをhttp.Serverに接続
 }
 
 func getBooks(ctx context.Context, c *gin.Context, client pb.BookServiceClient) {
 	fmt.Println("getBooks")
 	BookPb := pb.Book{}
-	res, err := client.GetBooks(context.Background(), &BookPb)
+	res, err := client.GetBooks(context.Background(), &BookPb) //ここでサーバ側へ接続している
 
 	if err != nil {
 		fmt.Println(err)
 	}
-	c.JSON(http.StatusOK, &res)
-	err = json.NewEncoder(os.Stdout).Encode(&res.BookList)
+	c.JSON(http.StatusOK, &res)                            //サーバから正常にレスポンスが返ってきた場合JSON形式で結果が表示される。→POSTMANを使用した際に戻り値がJSONで表示される。
+	err = json.NewEncoder(os.Stdout).Encode(&res.BookList) //pb.Book構造体をJSONに変換。「os.Stdout」→コンソール上で結果(JSON形式)を表示。
 	if err != nil {
 		fmt.Println(err)
 	}
 }
+
+//以下同様
+
 func getBook(ctx context.Context, c *gin.Context, client pb.BookServiceClient) {
 	fmt.Println("getBook")
 	id := c.Param("id")
@@ -103,7 +115,7 @@ func createBook(ctx context.Context, c *gin.Context, client pb.BookServiceClient
 }
 
 func updateBook(ctx context.Context, c *gin.Context, client pb.BookServiceClient) {
-	fmt.Println("crateBook")
+	fmt.Println("updateBook")
 	BookPb := pb.Book{}
 	if err := c.ShouldBindJSON(&BookPb); err != nil {
 		panic(err)
--- server/main.go
+++ server/main.go
@@ -11,38 +11,27 @@ import (
 	"google.golang.org/grpc"
 )
 
-type BookServiceClient struct {
-	pb.UnimplementedBookServiceServer
+// BookServerという構造体を定義
+type BookServer struct {
+	pb.UnimplementedBookServiceServer //gRPCサーバー実装時にUnimplementedBookServiceServerを埋め込んだ構造体を作る必要ある。
+	//こちらの構造体下記メソッドが定義されている。
 }
 
-type Book struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Author *Author `json:"author"`
-}
-
-type Author struct {
-	FirstName string
-	LastName  string
-}
-
-// var books []Book
-var Book2 Book
-
-func (c *BookServiceClient) GetBooks(ctx context.Context, req *pb.Book) (*pb.Books, error) {
+// サーバー側のメソッドを定義
+func (c *BookServer) GetBooks(ctx context.Context, req *pb.Book) (*pb.Books, error) {
 	fmt.Println("GetBooks was invoked")
 
 	repo := repository.NewRepository()
 	res, err := repo.FindAllBooks(ctx, req)
 	fmt.Println("res:", res)
 	if err != nil {
-		panic("error")
+		fmt.Println("err:", err)
 	}
 
 	return res, nil
 }
 
-func (c *BookServiceClient) GetBook(ctx context.Context, req *pb.Book) (*pb.Book, error) {
+func (c *BookServer) GetBook(ctx context.Context, req *pb.Book) (*pb.Book, error) {
 	fmt.Println("GetBook was invoked")
 
 	repo := repository.NewRepository()
@@ -55,7 +44,7 @@ func (c *BookServiceClient) GetBook(ctx context.Context, req *pb.Book) (*pb.Book
 	return res, nil
 }
 
-func (c *BookServiceClient) CreateBook(ctx context.Context, req *pb.Book) (*pb.Book, error) {
+func (c *BookServer) CreateBook(ctx context.Context, req *pb.Book) (*pb.Book, error) {
 	fmt.Println("CreateBook was invoked")
 	fmt.Println("req:", req)
 	repo := repository.NewRepository()
@@ -68,7 +57,7 @@ func (c *BookServiceClient) CreateBook(ctx context.Context, req *pb.Book) (*pb.B
 	return res, nil
 }
 
-func (c *BookServiceClient) UpdateBook(ctx context.Context, req *pb.Book) (*pb.Book, error) {
+func (c *BookServer) UpdateBook(ctx context.Context, req *pb.Book) (*pb.Book, error) {
 	fmt.Println("UpdateBook was invoked")
 	fmt.Println("req:", req)
 	repo := repository.NewRepository()
@@ -81,7 +70,7 @@ func (c *BookServiceClient) UpdateBook(ctx context.Context, req *pb.Book) (*pb.B
 	return res, nil
 }
 
-func (c *BookServiceClient) DeleteBook(ctx context.Context, req *pb.Book) (*pb.Books, error) {
+func (c *BookServer) DeleteBook(ctx context.Context, req *pb.Book) (*pb.Books, error) {
 	fmt.Println("DeleteBook was invoked")
 	fmt.Println("req:", req)
 	repo := repository.NewRepository()
@@ -96,19 +85,12 @@ func (c *BookServiceClient) DeleteBook(ctx context.Context, req *pb.Book) (*pb.B
 
 func main() {
 
-	// con, er := sql.Open("mysql", "root:@tcp(localhost:3306)/test?charset=utf8mb4")
-	// if er != nil {
-	// 	panic(er)
-	// }
-	// // defer con.Close() //DBの開放処理
-	// boil.SetDB(con)
-
-	lis, err := net.Listen("tcp", "localhost:8082")
+	lis, err := net.Listen("tcp", "localhost:8082") //tcp通信を設定。
 	if err != nil {
 		log.Fatalf("failed to list:%v", err)
 	}
-	s := grpc.NewServer()
-	pb.RegisterBookServiceServer(s, &BookServiceClient{})
+	s := grpc.NewServer()                          //gRPCサーバーを作成。sはServer構造体。
+	pb.RegisterBookServiceServer(s, &BookServer{}) //gRPCサーバーにサービスを登録→sにBookServerを登録。
 	fmt.Println("server is running")
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("fail to serve:%v", err)
